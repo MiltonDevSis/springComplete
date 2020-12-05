cb1dd831988cb9263d53a7f3fe72eb64c262d76b
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-04 22:11:56 -0300

    Projeto criado

---
+++ src/main/java/com/filho/milton/springcomplete/SpringcompleteApplication.java
@@ -0,0 +1,13 @@
+package com.filho.milton.springcomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filho/milton/springcomplete/SpringcompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filho.milton.springcomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
