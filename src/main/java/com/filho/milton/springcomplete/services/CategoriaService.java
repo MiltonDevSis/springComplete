68aea5c66caeddc57420995d36c8a1ce15d74cf7
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-06 17:48:58 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/filho/milton/springcomplete/services/CategoriaService.java
+++ src/main/java/com/filho/milton/springcomplete/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import com.filho.milton.springcomplete.domain.Categoria;
 import com.filho.milton.springcomplete.repositories.CategoriaRepository;
+import com.filho.milton.springcomplete.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,9 @@ public class CategoriaService {
     public Categoria buscar(Integer id) {
 
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
 
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+        "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+        
     }
 }
