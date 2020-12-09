6497016001a1c61d5193bf2774b8db7859774e9c
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-09 17:22:30 -0300

    Entrando no CRUD, Inserindo uma nova categoria

--- src/main/java/com/filho/milton/springcomplete/resources/CategoriaResource.java
+++ src/main/java/com/filho/milton/springcomplete/resources/CategoriaResource.java
@@ -1,14 +1,18 @@
 package com.filho.milton.springcomplete.resources;
 
+import java.net.URI;
+
 import com.filho.milton.springcomplete.domain.Categoria;
 import com.filho.milton.springcomplete.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -23,5 +27,13 @@ public class CategoriaResource {
         Categoria obj = service.buscar(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
