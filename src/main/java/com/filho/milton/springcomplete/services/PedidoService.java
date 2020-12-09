b17a79b61e4fa4d1854a1395abad9820dfa7bd6a
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-09 14:39:48 -0300

    End point {/pedidos} {/id} disponiveis

---
+++ src/main/java/com/filho/milton/springcomplete/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.filho.milton.springcomplete.services;
+
+import java.util.Optional;
+
+import com.filho.milton.springcomplete.domain.Pedido;
+import com.filho.milton.springcomplete.repositories.PedidoRepository;
+import com.filho.milton.springcomplete.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id) {
+
+        Optional<Pedido> obj = repo.findById(id);
+
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+        "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+        
+    }
+}
