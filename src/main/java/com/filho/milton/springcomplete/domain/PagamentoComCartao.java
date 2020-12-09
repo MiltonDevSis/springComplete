b718f5570b293dfcc56c870fd8877f92dc9bfebb
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-08 21:19:43 -0300

    Classes de pagamento com boleto e cartao

---
+++ src/main/java/com/filho/milton/springcomplete/domain/PagamentoComCartao.java
@@ -0,0 +1,28 @@
+package com.filho.milton.springcomplete.domain;
+
+import com.filho.milton.springcomplete.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento {
+    private static final long serialVersionUID = 1L;
+
+    private Integer numeroDePArcelas;
+
+    public PagamentoComCartao() {
+    }
+
+    public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+        super(id, estado, pedido);
+        this.numeroDePArcelas = numeroDeParcelas;
+    }
+
+    public Integer getNumeroDePArcelas() {
+        return numeroDePArcelas;
+    }
+
+    public void setNumeroDePArcelas(Integer numeroDePArcelas) {
+        this.numeroDePArcelas = numeroDePArcelas;
+    }
+
+    
+
+}
