f0285000e7d92919969bf885b911521380067c5b
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-11 15:56:51 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/filho/milton/springcomplete/dto/CategoriaDTO.java
+++ src/main/java/com/filho/milton/springcomplete/dto/CategoriaDTO.java
@@ -9,33 +9,35 @@ import com.filho.milton.springcomplete.domain.Categoria;
 import org.hibernate.validator.constraints.Length;
 
 public class CategoriaDTO implements Serializable {
-    private static final long serialVersionUID = 1L;
-
-    private Integer id;
-
-    @NotEmpty(message="Preenchimento obrigatório")
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
-    private String nome;
+	private String nome;
 
-    public CategoriaDTO(Categoria obj) {
-        id = obj.getId();
-        nome = obj.getNome();
-    }
+	public CategoriaDTO() {
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
+	public void setNome(String nome) {
+		this.nome = nome;
+	}	
 }
--- src/main/java/com/filho/milton/springcomplete/services/CategoriaService.java
+++ src/main/java/com/filho/milton/springcomplete/services/CategoriaService.java
@@ -22,46 +22,45 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria find(Integer id) {
-
+    public Optional<Categoria> find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-
-        return obj.orElseThrow(() -> new ObjectNotFoundException(
-        "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-        
-    }
-
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
+		return obj;
+	}
+	
 	public Categoria insert(Categoria obj) {
-        obj.setId(null);
+		obj.setId(null);
 		return repo.save(obj);
-    }
-    
-    public Categoria update(Categoria obj){
-        find(obj.getId());
-        return repo.save(obj);
-    }
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 
-    public void delete(Integer id){
-        find(id);
-        try{
-            repo.deleteById(id);
-        }
-        catch(DataIntegrityViolationException e){
-            throw new DataIntegrityException("Não é possível excluir uma categoria que possui Produtos");
-        }
-    }
-
-    public List<Categoria> findAll(){
-       return repo.findAll();
-    }
-
-    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-        return repo.findAll(pageRequest);
-    }
-
-    public Categoria fromDTO(CategoriaDTO objDto) {
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
