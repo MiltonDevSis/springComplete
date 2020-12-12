6ccaae78fd1fa43c886ac0c03b70fa361973d6ff
MiltonDevSis <milton.dev.sis@gmail.com>
2020-12-12 15:35:25 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/filho/milton/springcomplete/dto/ClienteNewDTO.java
+++ src/main/java/com/filho/milton/springcomplete/dto/ClienteNewDTO.java
@@ -2,20 +2,43 @@ package com.filho.milton.springcomplete.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import com.filho.milton.springcomplete.services.validation.ClienteInsert;
+
+import org.hibernate.validator.constraints.Length;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
     private static final long serialVersionUID = 1L;
-    
-    private String nome;
-    private String email;
-    private String cpfOuCnpj;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
+	private String nome;
+	
+	@NotEmpty
+    @Email(message = "Email inválido")
+	private String email;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String cpfOuCnpj;
+	
     private Integer tipo;
 
-    private String logradouro;
-    private String numero;
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String numero;
+	
     private String complemento;
-    private String bairro;
+	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
     private String cep;
 
+	@NotEmpty(message="Preenchimento obrigatório")
     private String telefone1;
     private String telefone2;
     private String telefone3;
